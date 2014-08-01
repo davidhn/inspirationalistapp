8e6a3cfd18effde0eaf281fb97bb2ff55cb3b4ba
theDavid <David@awesome.com>
2014-07-31 17:50:09 -0700

    changed from bootstrap grid to css slide desk and added modal to pins

--- inspirationalist.js
+++ inspirationalist.js
@@ -5,11 +5,28 @@ function myController($scope, $firebase) {
 	var dataRef = new Firebase("https://inspirationalistapp.firebaseio.com/");
 	$scope.interests = $firebase(dataRef);
 
-	$scope.interests.$on("loaded", function() {	
-		$scope.interests.$add(
-		{
-			description: "test description",
-			picture: "http://www.hollywoodreporter.com/sites/default/files/imagecache/modal_800/2014/05/carrey.jpg",
+	$scope.addPin = function() {
+		$scope.interests.$add({
+			"description": $scope.newDescription,
+			"picture": $scope.newPicture,
+			"video": $scope.newVideo
 		});
-	});
+		$scope.newDescription = "";
+		$scope.newPicture = "";
+		$scope.newVideo = "";
+		$('#myModal').modal('hide');
+	};
+
+	$scope.setModalPicture = function(picture) {
+			$scope.modalPicture = picture;
+
+	}
+
+	// $scope.interests.$on("loaded", function() {	
+	// 	$scope.interests.$add(
+	// 	{
+	// 		description: "test description",
+	// 		picture: "http://www.hollywoodreporter.com/sites/default/files/imagecache/modal_800/2014/05/carrey.jpg",
+	// 	});
+	// });
 };
