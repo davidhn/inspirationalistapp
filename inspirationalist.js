a07e2358708110c6b3ad8fc0530fb1fa156b614d
theDavid <David@awesome.com>
2014-08-04 09:55:03 -0700

    Favorite button is now working

--- inspirationalist.js
+++ inspirationalist.js
@@ -4,6 +4,7 @@ function myController($scope, $firebase) {
 
 	var dataRef = new Firebase("https://inspirationalistapp.firebaseio.com/");
 	$scope.interests = $firebase(dataRef);
+	console.log($scope.interests);
 
 	$scope.addPin = function() {
 		$scope.interests.$add({
@@ -23,11 +24,13 @@ function myController($scope, $firebase) {
 	}
 
 	$scope.clickFavorite = function(interest) {
-		var toggleFavorite = $firebase(dataRef);
-		console.log(interest.favorite);
-		toggleFavorite.$update({favorite: !interest.favorite});
-		console.log(interest.favorite);
-	}
+		var x = $scope.interests[interest.$id];
+		x.favorite = !interest.favorite;
+		$scope.interests.$save();
+		if (x.favorite == true) {
+
+		}
+	};
 };
 
 app.directive('myYoutube', function($sce) {
@@ -37,10 +40,8 @@ app.directive('myYoutube', function($sce) {
     replace: true,
     template: '<div style="width:100%;"><iframe style="overflow:hidden;height:100%;width:100%" width="100%" height="100%" src="{{url}}" frameborder="0" allowfullscreen></iframe></div>',
     link: function (scope) {
-        console.log(scope.code);
         var x = scope.code;
         y = x.split("=")[1];
-        console.log(y);
         scope.$watch('code', function (newVal) {
            if (y) {
                scope.url = $sce.trustAsResourceUrl("http://www.youtube.com/embed/" + y);
@@ -49,3 +50,23 @@ app.directive('myYoutube', function($sce) {
     }
   };
 });
+
+
+
+//*******************************
+//CODING THAT DID NOT WORK FOR FAVORITES FEATURE
+	// var child = $scope.interests.$child["favorite"];
+	// console.log($scope.interests[])
+
+	// interest.update
+	// var toggleFavorite = $firebase(dataRef);
+	// console.log(interest.favorite);
+	// toggleFavorite.update({favorite: !interest.favorite});
+	// console.log(interest.favorite);
+
+			// console.log(interest.favorite);
+	// interest.favorite = !interest.favorite;
+	// console.log(interest.favorite);
+	// console.log(interest);
+
+	// // interest.update({description: ""});
